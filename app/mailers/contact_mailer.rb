b33262b88bb0baaa2fead75ccc5e296f03f6709f
Christopher Olvera <chrisodeht@gmail.com>
2017-04-06 22:59:52 +0000

    Minor text fixes

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,12 +1,9 @@
-class ContactMailer < ActionMailer :: Base
-    default to: 'chrisodeht@gmail.com' 
-
-    def contact_email (name, email, body)
-    
-        @name = name
-        @email = email
-        @body = body
-        
-        mail(from: email, subject: 'Contact From message')
-    end
+class ContactMailer < ActionMailer::Base
+  default to: 'instructors@upskillcourses.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end

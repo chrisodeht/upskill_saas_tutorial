71564ce56576ac17162a2d5b8fbe325934f17617
Christopher Olvera <chrisodeht@gmail.com>
2017-05-08 23:11:50 +0000

    Added users controller and show view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502233632) do
+ActiveRecord::Schema.define(version: 20170503162226) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
